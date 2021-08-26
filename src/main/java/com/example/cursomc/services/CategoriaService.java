aa8f59ea1061de3169af093e98321af94cc67dfc
Karolina <karolinasouza@outlook.com>
2021-08-26 11:31:58 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -19,4 +19,8 @@ public class CategoriaService {
                 + ". Tipo: " + Categoria.class.getName()));
     }
 
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
