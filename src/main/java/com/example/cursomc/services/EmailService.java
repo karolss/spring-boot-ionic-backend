de799e9c0b4e11b166a0e0672d0a48f5790e8f16
Karolina <karolinasouza@outlook.com>
2021-09-16 10:05:44 -0300

    Email HTML

--- src/main/java/com/example/cursomc/services/EmailService.java
+++ src/main/java/com/example/cursomc/services/EmailService.java
@@ -3,9 +3,12 @@ package com.example.cursomc.services;
 import com.example.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
-
     void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
